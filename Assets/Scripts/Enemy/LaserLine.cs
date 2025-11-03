22b4e46a31c56f9d714cdc8067df36c4ba50911e
chloech314 <chloe.uoft314@gmail.com>
2025-11-02 22:16:56 -0500

    Line renderer is now used instead of a cylinder, allowing for the laser to stop at obstacles
    1. Set the obstacles in the prefab's array. The laser will stop when the raycast hits one of the objects in the list.
    2. The raycast will instakill any character that crosses its path

---
+++ Assets/Scripts/Enemy/LaserLine.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(LineRenderer))]
+public class LaserLine : MonoBehaviour
+{
+    public float maxLength = 20f;
+    public List<GameObject> obstacles;
+
+    private LineRenderer lineRenderer;
+
+    void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.startWidth = 0.3f;
+        lineRenderer.endWidth = 0.3f;
+        lineRenderer.positionCount = 2;
+        lineRenderer.useWorldSpace = false;
+    }
+
+    void Update()
+    {
+        UpdateLaser();
+    }
+
+    void UpdateLaser()
+    {
+        float laserLength = maxLength;
+        RaycastHit[] hits = Physics.RaycastAll(transform.position, transform.forward, maxLength);
+
+        System.Array.Sort(hits, (a, b) => a.distance.CompareTo(b.distance));
+
+        foreach (var hit in hits)
+        {
+            if (hit.collider.isTrigger)
+                continue;
+
+            GameObject hitObject = hit.collider.gameObject;
+
+            bool isPlayer =
+                hit.collider.GetComponentInParent<PlayerMech>() != null ||
+                hit.collider.GetComponentInParent<PlayerMouse>() != null;
+
+            if (isPlayer)
+            {
+                DamageReceiver damageReceiver = hit.collider.GetComponent<DamageReceiver>();
+                if (damageReceiver == null)
+                    damageReceiver = hit.collider.GetComponentInParent<DamageReceiver>();
+
+                if (damageReceiver != null)
+                    damageReceiver.ReceiveDamage(int.MaxValue, gameObject);
+
+                laserLength = hit.distance;
+                break;
+            }
+            else if (obstacles.Contains(hitObject))
+            {
+                laserLength = hit.distance;
+                break;
+            }
+        }
+
+        lineRenderer.SetPosition(0, Vector3.zero);
+        lineRenderer.SetPosition(1, Vector3.forward * laserLength);
+    }
+}
