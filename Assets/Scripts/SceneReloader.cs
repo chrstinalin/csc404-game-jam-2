3e990b43520f52da8f4334498df8bae28bcf34a6
chloech314 <chloe.uoft314@gmail.com>
2026-04-06 18:56:58 -0400

    Remapped controls
    Keyboard:
    a/d to change lock on target, e to shoot, tab to lock on, tab to summon mecha
    
    No more restarting the level with left joystick

--- Assets/Scripts/SceneReloader.cs
+++ Assets/Scripts/SceneReloader.cs
@@ -3,15 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class SceneReloader : MonoBehaviour
 {
-
-    void Update()
-    {
-        if (Input.GetButtonDown("ReloadScene"))
-        {
-            ReloadScene();
-        }
-    }
-
     public void ReloadScene()
     {
         Scene currentScene = SceneManager.GetActiveScene();
